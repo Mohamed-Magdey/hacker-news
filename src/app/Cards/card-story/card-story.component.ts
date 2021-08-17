c0f992b204a2aa1a58eb9fb04d77ace99c0c910e
mohamed <ragabmohame6@gmail.com>
2021-08-17 21:50:13 +0200

    Create card-story component

---
+++ src/app/Cards/card-story/card-story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-story',
+  templateUrl: './card-story.component.html',
+  styleUrls: ['./card-story.component.scss']
+})
+export class CardStoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
