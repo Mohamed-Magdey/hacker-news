2f600ceb2bd3d9b8c0a8a600faccb9a7ecf9a3b3
mohamed <ragabmohame6@gmail.com>
2021-08-18 13:07:07 +0200

    Create user service

---
+++ src/app/Cards/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import {environment} from '../../../environments/environment';
+// import {environment} from '../../../environments/environment.prod';
+import {User} from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/user/${id}.json`)
+  }
+}
